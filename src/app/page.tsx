597c730142023d912c35aa0590cc9047e2186f21
Lyall Coburn <lyallcoburn@Lyalls-MacBook-Air.local>
2026-01-30 22:05:52 +0200

    Update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           <div className="text-lg font-semibold tracking-tight">The Bureau •</div>
 
         
-            app opening soon →
+            Opening soon →
           
         </div>
 
